4a01932faf75a4b741bf383136805997d3d2427c
Ana-Morales <moralesanaruth@gmail.com>
2021-03-01 18:48:35 -0500

    Add CORS

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const multiparty = require('multiparty')
 const { days, blocks, slots, companies, mentors, schedule} = require('./db.js')
+const cors = require('cors')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use('/api', cors())
 
 const port = process.env.PORT || 3033
 
@@ -16,6 +18,19 @@ app.get('/api/mentors', (req, res) => {
 
 app.post('/api/mentors', (req, res) => {
     mentors.create(req.body).then(mentor => res.json(mentor))
+})
+
+app.post('/api/schedule', async (req, res) => {
+  const info = req.body
+  //console.log(info)
+  for (row of info) {
+    if (row.Name && row.Name.length > 0) {
+      const newMentor = {mentor: row.Name, email: row.Email}
+      //console.log(newMentor.mentor)
+      mentors.create(newMentor)
+    }
+  }
+  await mentors.findAll().then(mentors => res.json(mentors))
 })
 
 if(require.main === module) {
